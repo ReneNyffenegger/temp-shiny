db2d0059f90bb09ac618b11e0b09afd17367c7bb
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-17 09:07:38 +0200

    Create App.R

---
+++ dashboard/first/App.R
@@ -0,0 +1,74 @@
+
+library(shiny)
+library(shinydashboard)
+
+candyData <- read.table(
+text = '
+Brand       Candy           value
+Mars        100Grand        Choc1
+Netle       100Grand        Choc2
+Nestle      Crunch          Choc3',
+header           = TRUE,
+stringsAsFactors = FALSE)
+
+
+submenuUI <- function(id) {
+#  ns <- NS(id);
+   tagList(
+      box(title = "Data", status = "primary", solidHeader = T, width = 12,
+          fluidPage(
+             fluidRow(
+                column(2, offset = 0, style='padding:1px;', selectInput(NS("Select1"),"select1",unique(candyData$Brand))),
+                column(2, offset = 0, style='padding:1px;', selectInput(NS("Select2"),"select2",choices = NULL)),
+                column(2, offset = 0, style='padding:1px;', selectInput(NS("Select3"),"select3",choices = NULL ))
+             )
+          )
+      )
+   )
+}
+
+submenuServ <- function(input, output, session){
+
+   observeEvent(input$Select1, {
+     updateSelectInput(session,'Select2',
+     choices=unique(candyData$Candy[candyData$Brand==input$Select1]))
+   })
+
+   observeEvent(input$Select2,{
+      updateSelectInput(session,'Select3',
+      choices=unique(candyData$value[candyData$Brand==input$Select1 & candyData$Candy==input$Select2]))
+   })
+}
+
+ui <- dashboardPage(
+   dashboardHeader(),
+   dashboardSidebar(
+     sidebarMenu(
+        shinyjs::useShinyjs(),
+        id = "tabs",
+        menuItem(
+         'Item 1',
+          icon = icon("bar-chart-o"),
+          shinyjs::hidden(menuSubItem("dummy", tabName = "dummy")),
+
+          menuSubItem('Sub-item 1', tabName = 'subitem1'),
+          menuSubItem('Sub-item 2', tabName = 'subitem2')   # TODO -- Assign subitem2.
+        )
+      )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem("dummy"),
+      tabItem("subitem1", submenuUI('submenu1'))
+    )
+  )
+)
+
+server <- function(input, output,session) {
+  callModule(submenuServ, 'submenu1')
+}
+
+shinyApp(
+   ui     = ui,
+   server = server
+)
