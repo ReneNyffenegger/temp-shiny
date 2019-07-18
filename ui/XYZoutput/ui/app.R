077d63c7e3ef67c850d224e426a88575cfb2c72c
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-18 16:42:42 +0200

    add another number

--- ui/XYZoutput/ui/app.R
+++ ui/XYZoutput/ui/app.R
@@ -9,11 +9,18 @@ shinyApp(
    server = function(input, output, session) {
       output$dynamic_controls <- renderUI({
 
+      tagList(
          numericInput(
            inputId = 'i_num',
            label   = 'Enter a number',
            value   =  10
+         ),
+         numericInput(
+           inputId = 'i_num_2',
+           label   = 'Enter another number',
+           value   =  10
          )
+        );
 
       });
    }
