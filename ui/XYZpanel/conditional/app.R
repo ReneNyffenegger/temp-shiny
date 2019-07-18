74ac4c11640a4e9f70438f16da1d0f47d17998fc
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-18 12:59:06 +0200

    Update app.R

--- ui/XYZpanel/conditional/app.R
+++ ui/XYZpanel/conditional/app.R
@@ -3,11 +3,23 @@ ui <- fluidPage(
   selectInput(
      inputId = 'panel',
      label   = 'Choose Panel',
-     choices = c('diamonds', 'rock', 'pressure', 'cars')
+#    choices = c('diamonds', 'rock', 'pressure', 'cars')
+     choices = c('Panel one' = 'pnl_1', 'Panel two' = 'pnl_2', 'Panel three' = 'pnl_3')
   ),
 
   conditionalPanel(
-     condition    = "output.nrows",
+  #
+  #  conditionalPanel() wraps a set of UI elements that
+  #  then can be dynamically shown/hidden depending on
+  #  the value of a JavaScript expression
+  #
+  #  condition:
+  #      The JavaScript expression that determines
+  #      whether the widgets are shown.
+  #      The JavaScript objects(?) input and output
+  #      reflect the current respective values.
+  #
+     condition    = 'output.nrows',
      checkboxInput("headonly", "Only use first 1000 rows")
   )
 
@@ -18,10 +30,16 @@ server <- function(input, output, session) {
   INPUT <- reactive({
 
     switch(input$panel,
-           "rock"     = rock,
-           "pressure" = pressure,
-           "cars"     = cars
+#          'pnl_1'    = rock,
+           'pnl_2'    = pressure,
+           'pnl_3'    = cars
     )
+
+#   switch(input$panel,
+#          "rock"     = rock,
+#          "pressure" = pressure,
+#          "cars"     = cars
+#   )
 
   });
   
