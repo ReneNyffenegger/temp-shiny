8f1210c9b93c59b15ee7a4ad6fec768d82b6e2f2
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-19 10:12:39 +0200

    + shiny.trace

---
+++ options/shiny.trace/app.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+options(shiny.trace = TRUE);
+
+shinyApp(
+   ui = fluidPage (
+
+     actionButton(
+        inputId = 'go',
+        label   = 'Get time'
+     ),
+
+     verbatimTextOutput(outputId = 'tm')
+   ),
+
+   server = function(input, output, session) {
+
+     observeEvent(input$go, {
+       output$tm <- renderText(Sys.time());
+     });
+
+   }
+
+);
